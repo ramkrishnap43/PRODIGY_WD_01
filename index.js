edc6b2490a6b0d5a18e029510a3cd10a19e5c769
ramkrishnap43 <ramkrishnap.prabhu43@gmail.com>
2023-10-09 16:19:43 +0530

    final

---
+++ index.js
@@ -0,0 +1,8 @@
+// window.addEventListener("scroll", () => {
+//     const navbar = document.getElementById("check");
+//     if (window.scrollY > 50) {
+//         navbar.classList.add("scrolled");
+//     } else {
+//         navbar.classList.remove("scrolled");
+//     }
+// });
